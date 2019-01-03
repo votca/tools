c4f3ef25900b8941d17cf952e911c5f7e034a581
JoshuaSBrown <joshbro42867@yahoo.com>
2019-01-02 22:09:58 -0700

    Removed using namspace std from histogram.h and added scope std::

--- include/votca/tools/histogram.h
+++ include/votca/tools/histogram.h
@@ -26,8 +26,6 @@
 
 namespace votca { namespace tools {
 
-using namespace std;
-
 /**
     \brief class to generate histograms
 
@@ -57,7 +55,7 @@ class Histogram
         double getMax() const {return _max; }
         /// return the number of grid points
         double getN() const {return _options._n; }
-        vector<double> &getPdf() {return _pdf; }
+        std::vector<double> &getPdf() {return _pdf; }
         double getInterval() const { return _interval; }             
 
         void Normalize(void);
@@ -83,17 +81,17 @@ class Histogram
         };          
 
     private:        
-        vector<double> _pdf;
+        std::vector<double> _pdf;
         double _min, _max;
         double _interval;
         
         options_t _options;
 };
 
-inline ostream& operator<<(ostream& out, Histogram &h)
+inline std::ostream& operator<<(std::ostream& out, Histogram &h)
 {
     for(int i=0; i<h.getN(); i++) {
-        out << h.getMin() + h.getInterval()*((double)i+0.0) << " " << h.getPdf()[i] << endl;
+        out << h.getMin() + h.getInterval()*((double)i+0.0) << " " << h.getPdf()[i] << std::endl;
     }
     return out;
 }
